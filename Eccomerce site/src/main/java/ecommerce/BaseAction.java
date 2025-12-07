14814a83a9b38d0ebfab9fd4382801ef63b0f3cd
MuneebRouf03 <161604844+MuneebRouf03@users.noreply.github.com>
2025-12-07 11:19:08 +0000

    my jar files and my action java

---
+++ Eccomerce site/src/main/java/ecommerce/BaseAction.java
@@ -0,0 +1,30 @@
+package ecommerce;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Map;
+import com.opensymphony.xwork2.ActionSupport;
+import org.apache.struts2.interceptor.SessionAware;
+
+public class BaseAction extends ActionSupport implements SessionAware {
+    
+    protected Map<String, Object> session;
+    
+    // Simple database connection
+    protected Connection getConnection() throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(
+            "jdbc:mysql://localhost:3306/ecommerce_db",
+            "root", "root");
+    }
+    
+    // Check if user is logged in
+    protected boolean isLoggedIn() {
+        return session.get("loggedInUser") != null;
+    }
+    
+    @Override
+    public void setSession(Map<String, Object> session) {
+        this.session = session;
+    }
+}
